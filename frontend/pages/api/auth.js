8ecdcb81ee77c663299dadeae2cb2266650f4cc8
unknown <ahmadabdulkadir245@gmail.com>
2023-04-21 14:38:24 -0700

    revmede auth page in api

--- frontend/pages/api/auth.js
+++
@@ -1,9 +0,0 @@
-import {getSession} from 'next-auth/client'
-
-export default async function auth(req, res, next) {
-  const session = await getSession({ req });
-  if (!session) {
-    return res.status(401).end('Unauthorized');
-  }
-  next();
-}
